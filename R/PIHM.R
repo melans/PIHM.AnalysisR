0d2c30c08237a712711bf7787e9601a34be34c68
happynotes <lele.shu@gmail.com>
2015-04-15 16:14:14 -0400

    add PIHM.R file which is a toy/test code to show PIHM logo.

---
+++ R/PIHM.R
@@ -0,0 +1,26 @@
+#' PIHM Analysis project.
+#' Developed by Lele Shu( lele.shu@gmail.com  lzs157@psu.edu ) 
+#' 
+#' =============================================
+#'  Current version is for PIHM-MF or PIHM v2.4;
+#' 
+#' 
+#' 
+#' =============================================
+#' @param  Love PIHM? Defaults to TRUE.
+#' @keywords PIHM
+#' @export
+#' @examples
+#' PIHM()
+ 
+PIHM <-function(param=TRUE){
+    print ("########  #### ##     ## ##     ##");
+    print ("##     ##  ##  ##     ## ###   ###"); 
+    print ("##     ##  ##  ##     ## #### ####");
+    print ("########   ##  ######### ## ### ##");
+    print ("##         ##  ##     ## ##     ##");
+    print ("##         ##  ##     ## ##     ##"); 
+    print ("##        #### ##     ## ##     ##");
+    print ("    The Penn State Integrated Hydrologic Model");
+    print ("    Current version is PIHM-MF and PIHM v2.4");
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+# PIHM-AnalysisR
+PIHM analysis R packages.
+
+The project just starts. 
+
+Current version is applied for PIHM-MF, not useable for any PIHM before PIHM 2.4 since the output format are slightly different.
+
+Contacts Lele Shu(lele.shu@gmail.com or lzs157@psu.edu), you will get more information about this project.
