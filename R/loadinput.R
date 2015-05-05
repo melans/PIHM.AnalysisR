b3a227dc6f3dc3b4d0320522faa8edae51b3777a
happynotes <lele.shu@gmail.com>
2015-05-05 16:54:03 -0400

    clear loadinput(), which is empty function now

--- R/loadinput.R
+++ R/loadinput.R
@@ -13,32 +13,3 @@
 #' PIHM()
 
 
-goQ <-function(inpath, outpath){
-    nargin <- nargs();
-    if (nargin <1){
-        cat("\nUsage:\n\t data = readout(path_of_output,extension_of_data)\n");
-        cat("\n\n");
-        return(0);
-    }
-    
-
-}
-
-readout <-function(fn){
-    nargin <- nargs()
-    if (nargin <1 || nargin >1){
-        cat("\nUsage:\n\t data = readout(fn)\n");
-        cat("Where:\ndata = [T,data]\n");
-        cat("\n\n");
-        return(0);
-    }
-    
-    t <- as.Date(d[,1]);
-    data <- d[,-1];
-    mn <- dim(data)
-#    m <- mn[1];
-#    n <- mn[2];
-#    d <- data.frame(t,data,m,n);
-    d <- data.frame(t,data);
-    return(d);
-}
