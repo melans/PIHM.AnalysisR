f5e47fad48be540a57e8e280ca4bf79cb5837cbf
happynotes <lele.shu@gmail.com>
2015-05-13 12:26:44 -0400

    readUSGSgageLocal(fn) and readUSGSQ(siteid ,sdate,edate). The first reading USGS gage data ffrom the local file downloaded from USGS website.  The later one to utilize egret&dataRetrieval to download streamflow from USGS base on the siteid user provide.

---
+++ R/readOBS.R
@@ -0,0 +1,30 @@
+
+readUSGSgageLocal <- function (fn) {
+    lines <- readLines(fn);
+    lines <- lines[!is.na(lines)];
+    slongname =as.character(substr(lines[15],4,nchar(lines[15]) ) );
+    
+    rid <- which(grepl('^agency_cd',tolower(lines)))+2;
+
+    mat <- as.matrix(read.table(text=lines[rid:length(lines)]));
+    sname <- unique(mat[,2]);
+    t <- as.Date(mat[,3]);
+    ts <- xts(as.numeric(mat[,4])*0.3048^3,order.by=t)
+    colnames(ts) <- paste(sname,'m^3/s') ;
+    return( ts );
+}
+
+readUSGSQ <- function(siteid,sdate,edate){
+   # siteid <- '01576754'
+    loadinglib(c('EGRET','dataRetrieval'));
+    if(missing(bdate)){
+        sdate <- '1900-01-01';
+    }
+    if(missing(edate) ){
+        edate <- Sys.Date() ;
+    }
+    daily <- as.matrix(readNWISDaily(siteNumber=siteid,parameterCd='00060', startDate=sdate, endDate=edate));
+    t <- as.Date(daily[,'Date']);
+    ts <- xts(as.numeric(daily[,'Q']),order.by=t);
+    
+}
