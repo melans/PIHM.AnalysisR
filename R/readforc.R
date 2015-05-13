b925e25bb6f62f301acaa65e3f5df281c51c8f1f
happynotes <lele.shu@gmail.com>
2015-05-12 22:51:08 -0400

    readforc() which can load multi NUM_TS now. Problem is the performance. It is too slow, by now

--- R/readforc.R
+++ R/readforc.R
@@ -16,11 +16,12 @@
 
 readforc <-function(){
     if (pihmver > 2.3){
-        forcdata <- readforcmf();
+        forcing <- readforcmf();
     }else{
     }
+    
 
-    return(forcdata);
+    return(forcing);
 }
 
 #==================================================
@@ -35,10 +36,19 @@ readforcmf <- function(){
     lines <- readLines(forcfile);
    # message('\nFinish loading of .forc file')
     rid <- which(grepl('^num_meteo',tolower(lines)))
-    NumMeteoTS <- as.numeric(unlist(strsplit(lines[rid],' +'))[2]);
+    a <-scan(text =lines[rid], what=list('character','numeric'), quiet = TRUE)  
+    NumMeteoTS <- as.numeric(a[2]);
     
     rids <- which(grepl('^meteo_ts',tolower(lines)))
-    Forcing <- list();
+    data <- list();
+     
+    prcp <- list();
+    temp <- list();
+    rh <- list()
+    winds <- list();
+    solar <- list();
+    longw <- list();
+    press <- list();
     for (i in 1:NumMeteoTS){
         message('Parsing ',i,'/',NumMeteoTS,' sites\n');
         str <- unlist(strsplit(lines[rids[i]],' +'));
@@ -49,14 +59,53 @@ readforcmf <- function(){
         }else{
             sub <- lines[rids[i]+3:length(lines)]
         } 
-        tmplist <- scan(text = sub[!is.na(sub)] , what=list('character','character','numeric','numeric','numeric','numeric','numeric','numeric','numeric'),quiet = TRUE)
+        tmplist <- scan(text = sub[!is.na(sub)] ,
+                        what=list('character','character',
+                                  'numeric','numeric','numeric','numeric','numeric','numeric','numeric'),
+                        quiet = TRUE)
         t <- as.POSIXct(paste(unlist(tmplist[[1]]),unlist(tmplist[[2]])),format='%Y-%m-%d %H:%M',tz='UTC');
-        matdata <- matrix(as.numeric(unlist(tmplist[3:length(tmplist)])),ncol=7);
-        
-        ts <- xts(matdata,order.by = t);
-        Forcing[[i]] <- list("ID" = mid, "WindHeight" = windHeight, "ts" = ts);
+#        matdata <- matrix(as.numeric(unlist(tmplist[3:length(tmplist)])),ncol=7);
+        prcp[[i]]   <- xts(as.numeric(unlist(tmplist[3])),order.by = t);
+        temp[[i]]   <- xts(as.numeric(unlist(tmplist[4])),order.by = t);
+        rh[[i]]     <- xts(as.numeric(unlist(tmplist[5])),order.by = t);
+        winds[[i]]  <- xts(as.numeric(unlist(tmplist[6])),order.by = t);
+        solar[[i]]  <- xts(as.numeric(unlist(tmplist[7])),order.by = t);
+        longw[[i]]  <- xts(as.numeric(unlist(tmplist[8])),order.by = t);
+        press[[i]]  <- xts(as.numeric(unlist(tmplist[9])),order.by = t);
+        #data[[i]] <- list("ID" = mid, "WindHeight" = windHeight, "ts" = ts,"units"=Units);
     }
-    return(forc)
+    names(prcp )     <- paste('ts',as.character(1:NumMeteoTS),sep='')
+    names(temp )     <- paste('ts',as.character(1:NumMeteoTS),sep='')
+    names(rh   )     <- paste('ts',as.character(1:NumMeteoTS),sep='')
+    names(winds)     <- paste('ts',as.character(1:NumMeteoTS),sep='')
+    names(solar)     <- paste('ts',as.character(1:NumMeteoTS),sep='')
+    names(longw)     <- paste('ts',as.character(1:NumMeteoTS),sep='')
+    names(press)     <- paste('ts',as.character(1:NumMeteoTS),sep='')
+   # names(data)<- paste('TS',as.character(1:NumMeteoTS),sep='')
+    Units <- c("kg/m2/s","K","%","m/s","W/m2","W/m2","Pa")  
+    prcp$unit	<-	Units[1];
+    temp$unit	<-	Units[2];
+    rh$unit	    <-	Units[3];
+    winds$unit	<-	Units[4];
+    solar$unit	<-	Units[5];
+    longw$unit	<-	Units[6];
+    press$unit	<-	Units[7];
+   
+    rawForcing<-list(
+            'PRCP'  =	prcp    ,
+            'SFCTMP'=	temp    ,
+            'RH'    =	rh  ,
+            'SFCSPD'=	winds   ,
+            'SOLAR' =	solar   ,
+            'LONGWV'=	longw   ,
+            'PRES'  =	press ,
+            'NumMeteoTS'=NumMeteoTS);
+    
+   # names(rawForcing[1:7]) <- c("PRCP","SFCTMP","RH","SFCSPD","SOLAR","LONGWV","PRES")
+    #rawForcing$data<-data;
+    #rawForcing$NumMeteoTS <- NumMeteoTS;
+    #rawForcing$Unit <- c("kg/m2/s","K","%","m/s","W/m2","W/m2","Pa");
+    return(rawForcing)
 }
 #==================================================
 #       read forc for 2.0 version 
