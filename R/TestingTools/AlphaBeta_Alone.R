27d8c1601582af2fa05fe8d507a416912acb0cff
leonard-psu <lnl3@psu.edu>
2015-10-02 12:34:39 -0400

    AlphaBeta_Alone
    Modified version of Lele's AlphaBeta script that can be ran
    independently

---
+++ R/TestingTools/AlphaBeta_Alone.R
@@ -0,0 +1,64 @@
+#############################################################
+# Notes
+# Assumption is to specify wdir location of scripts
+#
+# To start Lele's Tool
+# source("AlphaBeta_Alone.R")
+#
+# See Notes for packages needed
+#############################################################
+
+## NEED THE FOLLOWING R FILES
+
+source("pihm.plot.R")
+source("KV_effective.R")
+
+#############################################################
+
+
+M=200
+x=seq(0.001, 1, length.out=M);
+N=10;
+K=5 #m/day
+
+
+BETA=seq(1.1,1.5,length.out=10)*1.5
+
+y=matrix(0,ncol=N,nrow=M)
+
+for(i in 1:N){
+    y[,i] = VGt2h(alpha=1,beta=BETA[i], se=x,minpsi=-70)#15e10);
+}
+
+col=terrain.colors(N)
+matplot(abs(y),x, type='l',log='x', lty=1, col=col, ylab='Effective Saturation', xlab='-H')
+leglines=paste('Beta=',round(BETA,3),sep='')
+legend('bottomleft',legend=leglines, lty=1, col=col)
+
+Keff=matrix(0,M,N)
+
+for(i in 1:N){
+    Keff[,i]=K*satKfunc(x,BETA[i])
+}
+
+col=terrain.colors(N)
+matplot(abs(Keff),x, type='l',log='x', lty=1, col=col, ylab='Effective Saturation', xlab='K(se)')
+leglines=paste('Beta',round(BETA,3),sep='')
+legend('bottomleft',legend=leglines, lty=1, col=col)
+
+Q=Keff*y
+col=terrain.colors(N)
+matplot(abs(Q),x, type='l',log='x', lty=1, col=col, ylab='Effective Saturation', xlab='Flow Rate')
+leglines=paste('Beta',round(BETA,3),sep='')
+legend('bottomleft',legend=leglines, lty=1, col=col)
+
+Alpha=seq(1,15,length.out=10)
+y=matrix(0,ncol=N,nrow=M)
+for(i in 1:N){
+    y[,i]=VGt2h(alpha=Alpha[i],beta=1.5, se=x,minpsi=-70)#15e10);
+}
+col=terrain.colors(N)
+matplot(abs(y),x, type='l',log='x', lty=1, col=col, ylab='Effective Saturation', xlab='-H')
+leglines=paste('Alpha=',round(Alpha,3),sep='')
+legend('bottomleft',legend=leglines, lty=1, col=col)
+
